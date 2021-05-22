3e1c11fd74a5767d7002eb33cb762912acb437de
Alan Fernandes <alanf3878@outlook.com>
2021-05-22 09:55:33 -0300

    exercicio1

---
+++ java.js
@@ -0,0 +1,251 @@
+//tipos de variaveis
+
+//var - variavel de escopo global
+
+//let - variável de escopo local
+
+//const - variável de valor constante
+
+//tipos simples de dados
+//string - dados textuais
+//number - dados numericos
+//boolean - dados de tipo verdadeiro ou falso
+
+//operadores de comparação
+// = atribuição
+// == verificar o valor 
+// === verificar o tipo e o valor
+// != verifica se os valores são diferentes
+// !== verificar se os tipos e os valores são diferentes
+// > maior que
+// > menor que 
+// >= maior ou igual a
+// <= menor que ou igual a 
+
+/*
+if (condição){
+    retornar tal tarefa
+}
+
+*/
+
+//if -se/ caso sim
+//else - se não/caso não
+
+
+//operadores lógicos 
+// && (and/e) - seu resultado é verdadeiro caso um E o outro sejam verdadeiros 
+// || (or/ou) - seu resultado é verdadeiro caso um OU o outro sejam verdadeiros
+// ! (not/inver) - Inverte o valor atribuído a ele 
+
+
+/*let nome = "Alan"
+let idade = 24
+let ano = 2021
+
+console.log ("Meu ano de nascimento é", idade - ano)
+
+
+let souhumano = true
+let gato = 1
+let cachorro = 3 
+
+console.log (gato + cachorro)*/
+
+
+/*
+
+
+if () {
+
+    nao for atendida
+
+} else{
+
+    for atendida
+}
+
+
+
+let idade = 18
+if(idade >= 18 ){
+    console.log("voce é maior de idade")
+
+} else{
+    console.log("você é maior de idade")
+
+}
+
+
+let nome = "marx"
+
+if(nome Length > 6)
+console.log("Seu nome tem mais de 6 letras")
+
+else{
+    console.log("Seu nome nao tem nem 6 letras!")
+}
+
+let nombre = "Marx"
+
+if(nombre.substring(0,1))  ==="L"){
+    console.log("Caiu no IF")
+
+} else{
+    console.log ("Caiu no else")
+} 
+
+
+
+function nomedaFunção () {
+    tarefa
+}
+
+
+nomedaFunção() 
+
+
+
+function oi (){
+    console.log("Óla")
+}
+
+oi( ) 
+
+function media (){
+    let n1 = 10;
+    let n2 = 8;
+    let result = (n1 + n2) / 2
+    console.log(result)
+}
+media()
+
+*/
+
+/**
+function mostrarNome (nome){
+   console.log(" Meu nome é "+ nome)  ( `Meu nome é ${nome}`) 
+}
+
+mostrarNome("Anísio")
+mostrarNome("Beatriz")
+mostrarNome("Leandro") 
+
+
+var age = 18
+function mostrarIdade (idade){
+   console.log("Eu tenho" + idade)
+}
+
+mostrarIdade(333) 
+
+function double(num){
+   return num*2
+}
+double(2);
+console.log(double(6))
+
+function soma (x, y){
+   return x+y
+}
+let reuslt = soma (6,7)
+
+console.log(soma (6,7))  
+
+
+function soma (x ,y){
+   return x=y
+}
+
+soma(2,6)
+
+console.log(soma (7, 9)) 
+
+
+let item = [1, 2, 3, 4, 5, 6]
+//               0      1    2
+let itens2 = ["Luiza", 22, true]
+
+console.log(itens2)
+
+    () //função ou método de manipulação da array
+[] //array - lista de dados
+{ } // objeto - coleção de dados
+
+
+// índice (index) é sempre contado do 0
+
+console.log(itens2[2], itens2[1])
+
+// console.log (`Meu nome é ${itens2 [2]}`) 
+
+
+//OBJETO
+// coleção de dados
+
+/* const nomeDaVariavel = {
+    propriedade: valor,
+    propriedade: valor,
+} 
+
+let aluna = {
+    nome :"Luiza",  //string
+    idade: 22,    //number
+    humana:true, //boolean
+    cabelo: "preto"
+}
+
+
+aluna.nome = ["Luiza", "Gonçalves"]
+aluna.cabelo = "chocolate"
+aluna.time = "Flamengo"
+delete aluna.time
+
+console.log("Olá ", aluna.nome[1]) 
+
+
+//ARRAY DE OBJETOS
+// listas de coleções de dados
+
+
+let alunos = [
+{
+    nome:"Luiza",
+    idade: 22,
+    humana: true,
+    cabelo: "preto",
+    amigos: ["amigo1", "amigo2", "amigo3"]
+},
+{
+    nome: "David",
+    idade: 22,
+    humana: false,
+    cabelo: "amarelo",
+    amigos: ["amigo1", "amigo2", "amigo3"]
+
+},
+
+{
+    nome:"Anísio",
+    idade: 22,
+    humana: true,
+    cabelo: "azul",
+    amigos: ["amigo1", "amigo2", "amigo3"]
+
+}
+]
+
+console.log(alunos[0].amigos[1]) */
+
+let nome = "Alan"
+let idade = 24
+let ano = 2021
+
+console.log ("Meu ano de nascimento é", idade - ano)
+
+
+let souhumano = true
+let gato = 1
+let cachorro = 3 
+
+console.log (gato + cachorro)
